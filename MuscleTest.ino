7012c4e7376e539627b6c81940b217064687f5b3
Mr-liruihao <mattoyt2008@gmail.com>
2024-07-12 21:24:34 -0400

    Add files via upload

---
+++ MuscleTest.ino
@@ -0,0 +1,39 @@
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  float value = analogRead(A0);
+  float signal = EMGFilter(value);
+  Serial.print(0); Serial.print(100);
+  Serial.println(signal);
+
+}
+
+float EMGFilter(float input) {
+    float output = input;
+    static float z1, z2;
+    float x = output - 0.05159732 * z1 - 0.36347401 * z2;
+    output = 0.01856301 * x + 0.03712602 * z1 + 0.01856301 * z2;
+    z2 = z1;
+    z1 = x;
+
+    x = output - -0.53945795 * z1 - 0.39764934 * z2;
+    output = 1.00000000 * x + -2.00000000 * z1 + 1.00000000 * z2;
+    z2 = z1;
+    z1 = x;
+
+    x = output - 0.47319594 * z1 - 0.70744137 * z2;
+    output = 1.00000000 * x + 2.00000000 * z1 + 1.00000000 * z2;
+    z2 = z1;
+    z1 = x;
+
+    x = output - -1.00211112 * z1 - 0.74520226 * z2;
+    output = 1.00000000 * x + -2.00000000 * z1 + 1.00000000 * z2;
+    z2 = z1;
+    z1 = x;
+
+    return output;
+}
