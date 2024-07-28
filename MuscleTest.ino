330da5a0fde0aae9c3830c5bd9b61c244fe3951a
Mr-liruihao <mattoyt2008@gmail.com>
2024-07-28 10:47:26 -0400

    Update MuscleTest.ino
    fixed the filter

--- MuscleTest.ino
+++ MuscleTest.ino
@@ -181,29 +181,36 @@ void loop() {
   Serial.println(signal);
 }
 
-float EMGFilter(float input) {
-    float output = input;
-    static float z1, z2;
-    float x = output - 0.05159732 * z1 - 0.36347401 * z2;
-    output = 0.01856301 * x + 0.03712602 * z1 + 0.01856301 * z2;
+float EMGFilter(float input)
+{
+  float output = input;
+  {
+    static float z1, z2; // filter section state
+    float x = output - 0.05159732*z1 - 0.36347401*z2;
+    output = 0.01856301*x + 0.03712602*z1 + 0.01856301*z2;
     z2 = z1;
     z1 = x;
-
-    x = output - -0.53945795 * z1 - 0.39764934 * z2;
-    output = 1.00000000 * x + -2.00000000 * z1 + 1.00000000 * z2;
+  }
+  {
+    static float z1, z2; // filter section state
+    float x = output - -0.53945795*z1 - 0.39764934*z2;
+    output = 1.00000000*x + -2.00000000*z1 + 1.00000000*z2;
     z2 = z1;
     z1 = x;
-
-    x = output - 0.47319594 * z1 - 0.70744137 * z2;
-    output = 1.00000000 * x + 2.00000000 * z1 + 1.00000000 * z2;
+  }
+  {
+    static float z1, z2; // filter section state
+    float x = output - 0.47319594*z1 - 0.70744137*z2;
+    output = 1.00000000*x + 2.00000000*z1 + 1.00000000*z2;
     z2 = z1;
     z1 = x;
-
-    x = output - -1.00211112 * z1 - 0.74520226 * z2;
-    output = 1.00000000 * x + -2.00000000 * z1 + 1.00000000 * z2;
+  }
+  {
+    static float z1, z2; // filter section state
+    float x = output - -1.00211112*z1 - 0.74520226*z2;
+    output = 1.00000000*x + -2.00000000*z1 + 1.00000000*z2;
     z2 = z1;
     z1 = x;
-
-    return output;
+  }
+  return output;
 }
-
